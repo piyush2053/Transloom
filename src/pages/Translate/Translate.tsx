118e46a1bbbdd2217a11293ac8419d08e1647ed8
piyush.patel <piyush.patel@rws.com>
2024-03-08 01:43:13 +0530

    Translate 1.0

--- src/pages/Translate/Translate.tsx
+++ src/pages/Translate/Translate.tsx
@@ -1,61 +1,58 @@
 import React, { useRef, useState } from 'react';
 import { FaMagic } from "react-icons/fa";
-import { Card, CardContent, Typography } from '@mui/material';
-import '../../index.css';
-import Lottie from 'react-lottie'
-import animationData from './loader/loder.json'
-import animationDataPopper from './loader/poppers.json'
-import animationDataWarning from './loader/warning.json'
+import { Card, CardContent, MenuItem, Typography } from '@mui/material';
+import Select from '@mui/material/Select';
+import Lottie from 'react-lottie';
+import animationData from './loader/loder.json';
+import animationDataPopper from './loader/poppers.json';
+import languages from './LanguagesDropdown.ts';
 
 const Translate = () => {
   const inputRef = useRef(null);
-  const [definitions, setDefinitions] = useState<{ word: string, definition: string, example: string }[]>([]);
-  const [loading, setloading] = useState(false);
+  const [definitions, setDefinitions] = useState('');
+  const [loading, setLoading] = useState(false);
   const [poppers, setPoppers] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
   const [warning, setWarning] = useState(false);
 
   const handleButtonClick = () => {
-    setloading(true)
+    setLoading(true);
     const inputValue = inputRef.current.value;
-    const queryParams = `?term=${encodeURIComponent(inputValue)}`;
-    const headers = {
-      'X-RapidAPI-Key': 'f44f0eb869msh9cfe82dc6116de7p1c6861jsne92874139af1',
-      'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
-      'Content-Type': 'application/json'
-    };
-    fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define${queryParams}`, {
-      method: 'GET',
-      headers: headers,
+
+    fetch('https://google-translation-unlimited.p.rapidapi.com/translate', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Key': 'f44f0eb869msh9cfe82dc6116de7p1c6861jsne92874139af1',
+        'X-RapidAPI-Host': 'google-translation-unlimited.p.rapidapi.com'
+      },
+      body: new URLSearchParams({
+        texte: inputValue,
+        to_lang: selectedLanguage
+      })
     })
       .then(response => response.json())
       .then(data => {
-        setPoppers(true)
-        setloading(false)
+        setPoppers(true);
+        setLoading(false);
+        setDefinitions(data.translation_data.translation);
+        setSelectedLanguage('')
         setTimeout(() => {
-          setDefinitions(data.list);
-        }, 1000)
-        setTimeout(() => {
-          setPoppers(false)
-        }, 3000)
+          setPoppers(false);
+        }, 3000);
       })
       .catch(error => {
-        setPoppers(false)
-        setloading(false)
+        setPoppers(false);
+        setSelectedLanguage('')
+        setLoading(false);
         console.error('Error:', error);
       });
-  }
+  };
+
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       setWarning(false)
       handleButtonClick();
-    } else {
-      const trimmedValue = inputRef.current.value.trim();
-      if (trimmedValue.includes(' ') && event.key !== 'Backspace' && event.key !== 'Shift' && event.key !== 'ArrowLeft' && event.key !== 'Control' && !(event.ctrlKey && event.key === 'a')) {
-        setWarning(true)
-        event.preventDefault();
-      } else {
-        setWarning(false)
-      }
     }
   }
 
@@ -63,14 +60,6 @@ const Translate = () => {
     loop: true,
     autoplay: true,
     animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
-  const defaultOptionsWarning = {
-    loop: true,
-    autoplay: true,
-    animationData: animationDataWarning,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
     }
@@ -84,80 +73,75 @@ const Translate = () => {
     }
   };
 
+  const selectedLanguageLabel = languages.find(lang => lang.value === selectedLanguage)?.label;
+
+
   return (
     <div className="fade-in text-white py-16 px-4 md:px-0 h-screen">
       <div className="max-w-7xl mx-auto flex flex-col items-center text-center">
         <div className="flex items-center justify-center mb-4">
           <span className="bg-transparent text-[#fff] text-xs font-semibold px-5 py-1 rounded-full flex border border-white">
             <FaMagic className="text-[#fff] text-1xl mr-1 mt-0.5" />
-            Step inside the box where the magic of content creation unfolds
+            Step inside the box where the magic of content Translations unfolds
           </span>
         </div>
         <h1 className="text-7xl font-bold mb-6">
-          Content Dictionary
+          Translations
         </h1>
       </div>
       <div className={`mx-5 p-1 mt-10 rounded-xl bg-[#141414] ${warning ? 'h-[120px]' : 'h-[100px]'}`}>
-        <input ref={inputRef} type='text' className='flex-grow bg-transparent rounded border border-none p-7' placeholder='Give me a Word Please ' onKeyDown={handleKeyPress}></input>
-        {warning ? <div className='fade in flex ml-3'><Typography className='fade in inverseColor text-bold' >Single Word Please</Typography></div> : null}
+        <input ref={inputRef} type='text' className='flex-grow bg-transparent rounded border border-none p-7' placeholder='Enter Your Query' onKeyDown={handleKeyPress}></input>
       </div>
       <div className="max-w-7xl mx-auto flex flex-col items-center text-center mt-5">
-        {loading
-          ?
-          <div> <Lottie
-            options={defaultOptions}
-            height={50}
-            width={50}
-          /></div>
-          :
-          <>{warning ? <div className='fade in flex justify-end'><Lottie
-            options={defaultOptionsWarning}
-            height={60}
-            width={60}
-          /></div> : <button className='fade in bg-[#fff] flex text-[#8c8c8c] text-[20px] font-semibold px-4 py-1 rounded-full border border-white transition-all duration-300 hover:bg-transparent hover:text-[#fff] hover:animate-pulse' onClick={handleButtonClick}>
-            Magic <FaMagic className='ml-2 mt-1' />
-          </button>}</>
-        }
+        {selectedLanguage ? (
+          loading ? (
+            <Lottie
+              options={defaultOptions}
+              height={50}
+              width={50}
+            />
+          ) : (
+            <button className='fade in bg-[#fff] flex text-[#8c8c8c] text-[20px] font-semibold px-4 py-1 rounded-full border border-white transition-all duration-300 hover:bg-transparent hover:text-[#fff] hover:animate-pulse' onClick={handleButtonClick}>
+              Translate to {selectedLanguageLabel} <FaMagic className='ml-2 mt-1' />
+            </button>
+          )
+        ) : (
+          <div>
+            <Select
+              value={selectedLanguage}
+              labelId="demo-simple-select-helper-label"
+              id="demo-simple-select-helper"
+              label="Language"
+              className='w-[200px] bg-white'
+              onChange={(e) => setSelectedLanguage(e.target.value)}
+            >
+              {languages.map((lang) => (
+                <MenuItem key={lang.value} value={lang.value}>{lang.label}</MenuItem>
+              ))}
+            </Select>
+          </div>
+        )}
+      </div>
+      <div className="max-w-7xl mx-auto flex flex-col items-center text-center mt-2">
+        {selectedLanguage ? <button onClick={() => setSelectedLanguage('')}>Change language</button> : null}
       </div>
       <div className="fade-in max-w-7xl mx-auto mt-5 px-7" style={{ position: "relative" }}>
         <div className="poppers" style={{ position: 'absolute', top: 0, left: '50%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
-          {poppers ? <Lottie
+          {poppers && <Lottie
             options={popperOptions}
             height={500}
             width={500}
-          /> : null}
+          />}
         </div>
-        <div className="poppers" style={{ position: 'absolute', top: 0, left: '0', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
-          {poppers ? <Lottie
-            options={popperOptions}
-            height={500}
-            width={500}
-          /> : null}
-        </div>
-        <div className="poppers" style={{ position: 'absolute', top: 0, left: '100%', transform: 'translate(-50%, -50%)', zIndex: 999 }}>
-          {poppers ? <Lottie
-            options={popperOptions}
-            height={500}
-            width={500}
-          /> : null}
-        </div>
-        {definitions.map((definition, index) => (
-          <Card key={index} className="mt-3 rounded-full" sx={{ borderRadius: "20px", backgroundColor: "#1E1E1E" }}>
-            <CardContent >
-              <Typography variant="h5" component="div" className='text-[#757575]'>
-                {definition.word}
-              </Typography>
+        {definitions && (
+          <Card className="mt-3 rounded-full" sx={{ borderRadius: "20px", backgroundColor: "#1E1E1E" }}>
+            <CardContent>
               <Typography sx={{ mb: 1.5 }} className='text-[#757575]'>
-                {definition.definition}
+                {definitions}
               </Typography>
-              <div className='py-2 pl-2 bg-[#616161] rounded-lg'>
-                <Typography variant="body2" className='text-[#E0E0E0]'>
-                  Example: {definition.example}
-                </Typography>
-              </div>
             </CardContent>
           </Card>
-        ))}
+        )}
       </div>
     </div>
   );
